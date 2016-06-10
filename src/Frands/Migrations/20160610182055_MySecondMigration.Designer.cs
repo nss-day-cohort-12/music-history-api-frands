435be3263b7afe786d787cb850272c51bb2a3946
Mark Williams <markzawill@gmail.com>
2016-06-10 13:50:07 -0500

    initial commit

---
+++ src/Frands/Migrations/20160610182055_MySecondMigration.Designer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Frands.Models;
+
+namespace Frands.Migrations
+{
+    [DbContext(typeof(FrandsDbContext))]
+    [Migration("20160610182055_MySecondMigration")]
+    partial class MySecondMigration
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.0.0-rc2-20901")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("Frands.Models.Album", b =>
+                {
+                    b.Property<int>("AlbumId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("AlbumName");
+
+                    b.Property<string>("AlbumsHref");
+
+                    b.Property<string>("Artist");
+
+                    b.Property<string>("YearReleased");
+
+                    b.HasKey("AlbumId");
+
+                    b.ToTable("Album");
+                });
+
+            modelBuilder.Entity("Frands.Models.Track", b =>
+                {
+                    b.Property<int>("TrackId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("AlbumId");
+
+                    b.Property<string>("Artist");
+
+                    b.Property<string>("Title");
+
+                    b.Property<string>("TracksHref");
+
+                    b.HasKey("TrackId");
+
+                    b.ToTable("Track");
+                });
+        }
+    }
+}
---
+++ src/Frands/Models/Track.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Frands.Models
+{
+    public class Track
+    {
+        public int TrackId { get; set; }
+        public string Title { get; set; }
+        public int AlbumId { get; set; }
+        public string Artist { get; set; }
+        public string TracksHref { get; set; }
+
+
+    }
+}
